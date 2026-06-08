5d6fe991dfad4656f52dc28400f1b532eea886df
KSJ <83096833+KSJDiamondGaming@users.noreply.github.com>
2026-06-08 22:45:16 +0100

    Render mini audio controls globally

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import FloatingNotice from './components/FloatingNotice'
+import AudioControls from './components/AudioControls'
 
 // Pages
 import Home from './pages/Home'
@@ -49,6 +50,7 @@ function App() {
 
       <Footer />
       <FloatingNotice />
+      <AudioControls />
     </BrowserRouter>
   )
 }
