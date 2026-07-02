08ec6e5c77ded575def844d4149ad57ef3d102d4
KSJ Hub <sycomorgan@gmail.com>
2026-07-02 02:58:00 +0100

    Remove obsolete merch media data file

--- src/data/merchMedia.js
+++
@@ -1,9 +0,0 @@
-import { merchProducts } from './merchProducts'
-
-// Compatibility export for the current Merch page.
-// Product and image data now have one source of truth in merchProducts.js.
-export const merchMedia = Object.fromEntries(
-  merchProducts
-    .filter((product) => product.image?.id)
-    .map((product) => [product.image.id, product.image]),
-)
